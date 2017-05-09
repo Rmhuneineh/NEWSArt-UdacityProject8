d544d0c34f57ba82a4a4d96d7a9622ba79104718
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-05-09 02:25:31 +0200

    Add webview UI and code

---
+++ app/src/main/java/com/example/android/newsart/WebViewActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.newsart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        Intent intent = getIntent();
+        String url = intent.getData().toString();
+        webView.loadUrl(url);
+    }
+}
