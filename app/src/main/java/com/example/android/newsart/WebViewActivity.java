52381eaf3dddd4c0edc28eea1d69cc48fff21e50
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-05-11 19:05:07 +0200

    Set webview client

--- app/src/main/java/com/example/android/newsart/WebViewActivity.java
+++ app/src/main/java/com/example/android/newsart/WebViewActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 public class WebViewActivity extends AppCompatActivity {
 
@@ -14,6 +15,8 @@ public class WebViewActivity extends AppCompatActivity {
         WebView webView = (WebView) findViewById(R.id.web_view);
         Intent intent = getIntent();
         String url = intent.getData().toString();
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.web_progress_bar);
+        webView.setWebViewClient(new ArticleWebViewClient(progressBar));
         webView.loadUrl(url);
     }
 }
