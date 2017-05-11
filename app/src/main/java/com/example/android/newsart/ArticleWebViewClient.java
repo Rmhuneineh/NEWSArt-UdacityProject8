d6f68b92b3fa33e144b6111b92b257842da31992
Ragheed Ma'rouf Huneineh <ragheedmhuneineh@outlook.com>
2017-05-11 19:04:27 +0200

    Remove extra method

--- app/src/main/java/com/example/android/newsart/ArticleWebViewClient.java
+++ app/src/main/java/com/example/android/newsart/ArticleWebViewClient.java
@@ -18,12 +18,6 @@ public class ArticleWebViewClient extends WebViewClient {
     }
 
     @Override
-    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        view.loadUrl(url);
-        return true;
-    }
-
-    @Override
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
         mProgressBar.setVisibility(View.GONE);
